5f4abc0d740423e92d61928bf0f451645fe4798b
unknown <abilpach74@gmail.com>
2018-09-17 23:59:02 +0430

    python

---
+++ circle_area.py
@@ -0,0 +1,6 @@
+#a program to calculate area of the circle.
+radius=float(input("please enter radius of circle:"))
+p=3.141516
+rpt=radius*radius
+circle_area=rpt*p
+print("area of the circle is:",circle_area)
